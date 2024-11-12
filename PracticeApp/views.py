637d987644f7cf344803d6e8170a77a268686027
Michael254-hub <michaelsimiyu54@gmail.com>
2024-11-12 06:06:22 +0300

    first commit

---
+++ PracticeApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request,'contacts.html')
+def blog(request):
+    return render(request,'blog.html')
+def services(request):
+    return render(request,'services.html')
